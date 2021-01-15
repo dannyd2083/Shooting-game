ba81438fc846338656b3d6e71b75e217119df97c
dannyd2083 <58844507+dannyd2083@users.noreply.github.com>
2021-01-14 22:56:45 -0800

    set up enemy
    set up the pathfinding
    by using NavMeshAgent

---
+++ New Idea/Assets/Script/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent  (typeof (NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent pathfinder;
+    Transform target;
+
+    void Start()
+    {
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pathfinder.SetDestination(target.position);
+    }
+}
